cf1e15d9351594fdf808c559a2b0810092f12045
MuhammadHafijur <85216180+MuhammadHafijur@users.noreply.github.com>
2024-08-30 16:16:13 +0600

    first commit

---
+++ composables/useControls.js
@@ -0,0 +1,31 @@
+// composables/useImageControls.js
+import { useFetch } from '#app';
+
+import { ref } from "vue";
+
+const showPreview = ref(true);
+const aspectRatio = ref("square");
+const bgLayerOpacity = ref(40);
+const bgLayerColor = ref("#00C16A");
+const mixBlendValue = ref("normal");
+const sliderBackground = ref("");
+
+const useImageControls = () => {
+  const { data, refresh } = useFetch('/api/count');
+
+  const updateCount = async () => {
+    if (data.value) {
+      const currentCount = data.value.count;
+      const updatedCount = currentCount + 1;
+      await $fetch('/api/count', {
+        method: 'POST',
+        body: { count: updatedCount }
+      });
+      await refresh(); // Refresh the count from the API
+    }
+  };
+
+  return { updateCount, data };
+};
+
+export default useImageControls;
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Current Count: {{ data?.count }}</h1>
+    <button @click="updateCount">Increment</button>
+  </div>
+</template>
+
+<script setup>
+import useImageControls from '../composables/useControls.js';
+
+const { updateCount, data } = useImageControls();
+</script>
